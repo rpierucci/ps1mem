8b48de53c55e0adf4b749b6e70ffa2300aba44ac
Robert Pierucci <pierucci.robert@gmail.com>
2020-04-20 23:51:58 -0700

    Treeview, restructuring of gui

--- mc.py
+++ mc.py
@@ -1,132 +1,162 @@
 import tkinter as tk
+from tkinter import *
 from tkinter import filedialog
+from tkinter.ttk import *
 import os
 import io
 import codecs
 import binascii
 import array
 
-folderPath = 'C:/'
-root = tk.Tk()
-folderPathLabel = tk.Label(root, text=folderPath)
-files = []
-
-def loadConfig():
+def loadConfig(folderPath):
     try:
         file = open(os.path.join(os.path.dirname(__file__), 'settings.txt'), 'r')
         with file:
-            folderPathLabel['text'] = file.readline()
+            gui.folderPath = file.readline()
+            gui.folderPathLabel['text'] = file.readline()
+        file.close()
     except:
         print('settings not found')
-        pass
 
 def setMemoryCardDirectory():
-    folderPath = filedialog.askdirectory(initialdir='C:/')
+    gui.folderPath = filedialog.askdirectory(initialdir=gui.folderPathLabel['text'])
     file = open(os.path.join(os.path.dirname(__file__), 'settings.txt'), 'w')
-    file.write(folderPath)
-    folderPathLabel['text'] = folderPath
-    # for files in os.walk(folder):
-    #     print(files)
+    file.write(gui.folderPath)
+    file.close()
+    gui.folderPathLabel['text'] = gui.folderPath
 
 def scanMemoryCards():
-    countryCode = None
-    productCode = None
-    identifier = None
-    iconDisplayFlag = None # 00 = no icon, 11 = icon has 1 frame (static), 12 = icon has 2 frames (animated), 13 = icon has 3 frames
-    blockCount = None
-    saveTitle = None
-    bitmap = None
+    for directory, subdirectory, files in os.walk(gui.folderPath):
+        for f in files:
+            if '.mcr' in f:
+                file = open(directory + '/' + f, 'rb')
+                countryCode = None
+                productCode = None
+                identifier = None
+                iconDisplayFlag = None # 00 = no icon, 11 = icon has 1 frame (static), 12 = icon has 2 frames (animated), 13 = icon has 3 frames
+                blockCount = None
+                saveTitle = None
+                bitmap = None
 
-    file = open(os.path.join(os.path.dirname(__file__), 'test.mcr'), 'rb')
-    with file:
-        file.seek(0x0008A)
-        byte = file.read(2)
-        countryCode = byte
+                with file:
+                    file.seek(0x0008A)
+                    byte = file.read(2)
+                    countryCode = byte.decode('utf8')
 
-        file.seek(0x0008C)
-        byte = file.read(10)
-        productCode = byte
+                    file.seek(0x0008C)
+                    byte = file.read(10)
+                    productCode = byte.decode('utf8')
 
-        file.seek(0x00096)
-        byte = file.read(8)
-        identifier = byte
+                    file.seek(0x00096)
+                    byte = file.read(8)
+                    identifier = byte.decode('utf8')
 
-        file.seek(0x02002)
-        byte = file.read(1)
-        iconDisplayFlag = byte
+                    file.seek(0x02002)
+                    byte = file.read(1)
+                    iconDisplayFlag = byte.decode('utf8')
 
-        file.seek(0x02003)
-        byte = file.read(1)
-        blockCount = binascii.hexlify(byte)
+                    file.seek(0x02003)
+                    byte = file.read(1)
+                    blockCount = binascii.hexlify(byte)
 
-        # file.seek(0x02004)
-        # byte = file.read(64)
-        # saveTitle = byte
+                    # file.seek(0x02004)
+                    # byte = file.read(64)
+                    # saveTitle = byte
 
-        file.seek(0x02060)
-        byte = file.read(1)
-        palette = byte
-        hexadecimal = binascii.hexlify(byte)
-        decimal = int(hexadecimal, 16)
-        binary = bin(decimal)[2:].zfill(8)
+                    file.seek(0x02060)
+                    byte = file.read(1)
+                    palette = byte
+                    hexadecimal = binascii.hexlify(byte)
+                    decimal = int(hexadecimal, 16)
+                    binary = bin(decimal)[2:].zfill(8)
 
-        print((0b10101100) & 0x1F << 3)
-        print(((0b11111001) & 0x3 << 6) | ((0b10101100) & 0xE0 >> 2))
-        print(((0b11111001) & 0x7C) << 1)
-        print(((0b11111001) & 0x80))
+                    # print((0b10101100) & 0x1F << 3)
+                    # print(((0b11111001) & 0x3 << 6) | ((0b10101100) & 0xE0 >> 2))
+                    # print(((0b11111001) & 0x7C) << 1)
+                    # print(((0b11111001) & 0x80))
 
-        # file.seek(0x02080)
-        # byte = file.read(128)
-        # bitmap = byte
-        # hex values = every pair is flipped, each number represents pixel in table
-        
-        # hexadecimal = binascii.hexlify(byte)
-        # decimal = int(hexadecimal, 16)
-        # binary = bin(decimal)[2:].zfill(8)
-        # print("hex: %s, decimal: %s, binary: %s" % (hexadecimal, decimal, binary))
+                    # file.seek(0x02080)
+                    # byte = file.read(128)
+                    # bitmap = byte
+                    # hex values = every pair is flipped, each number represents pixel in table
+                    
+                    # hexadecimal = binascii.hexlify(byte)
+                    # decimal = int(hexadecimal, 16)
+                    # binary = bin(decimal)[2:].zfill(8)
+                    # print("hex: %s, decimal: %s, binary: %s" % (hexadecimal, decimal, binary))
+                gui.updateTable(f, countryCode, productCode, identifier)
 
 
+                # print("Country Code: " + str(countryCode))
+                # print("Product Code: " + str(productCode))
+                # print("Identifier: " + str(identifier))
+                # print("File Name: " + str(countryCode) + str(productCode) + str(identifier))
+                # print("Icon Display Flag: " + str(iconDisplayFlag))
+                # print("Block Count: " + str(blockCount))
+                # print "Save Title: " + saveTitle
+                # print "Bitmap: " + bitmap
 
-    print("Country Code: " + str(countryCode))
-    print("Product Code: " + str(productCode))
-    print("Identifier: " + str(identifier))
-    print("File Name: " + str(countryCode) + str(productCode) + str(identifier))
-    print("Icon Display Flag: " + str(iconDisplayFlag))
-    print("Block Count: " + str(blockCount))
-    # print "Save Title: " + saveTitle
-    # print "Bitmap: " + bitmap
+                file.close()
 
-    file.close()
 
 def exitProgram():
-    root.quit()
+    gui.master.quit()
 
 def about():
     pass
+class App():
+    folderPathLabel = None
+    folderPath = 'C:/'
 
-def window(root):
-    root.title('PS1 Mem Organizer')
-    root.geometry('1280x720')
+    def __init__(self):
+        self.master = tk.Tk()
+        self.master.title('PS1Mem')
+        self.master.geometry('1280x720')
+        self.createUI()
+        self.master.grid_rowconfigure(0, weight = 1)
+        self.master.grid_columnconfigure(0, weight = 1)
 
-    menu = tk.Menu(root)
-    fileMenu = tk.Menu(menu, tearoff=0)
-    fileMenu.add_command(label='Set MCR Directory', command=setMemoryCardDirectory)
-    fileMenu.add_command(label='Scan MCR Files', command=scanMemoryCards)
-    fileMenu.add_command(label='Exit', command=exitProgram)
-    menu.add_cascade(label='File', menu=fileMenu)
+    def createUI(self):
+        self.menu = tk.Menu(self.master)
+        self.fileMenu = tk.Menu(self.menu, tearoff=0)
+        self.fileMenu.add_command(label='Set MCR Directory', command=setMemoryCardDirectory)
+        self.fileMenu.add_command(label='Scan MCR Files', command=scanMemoryCards)
+        self.fileMenu.add_command(label='Exit', command=exitProgram)
+        self.menu.add_cascade(label='File', menu=self.fileMenu)
+        self.helpMenu = tk.Menu(self.menu, tearoff=0)
+        self.helpMenu.add_command(label='About', command=about)
+        self.menu.add_cascade(label="Help", menu=self.helpMenu)    
+        self.master.config(menu=self.menu)
+        self.folderPathLabel = tk.Label(self.master, text=self.folderPath)
+        self.folderPathLabel.place(relx=1.0, rely=1.0, anchor='se')
 
-    helpMenu = tk.Menu(menu, tearoff=0)
-    helpMenu.add_command(label='About', command=about)
-    menu.add_cascade(label="Help", menu=helpMenu)
+        tv = Treeview(self.master)
+        tv['columns'] = ['countryCode', 'productCode', 'identifier']
+        tv.column('#0', width = 200)
+        tv.column('countryCode', width = 200)
+        tv.column('productCode', width = 200)
+        tv.column('identifier', width = 200)
+        tv.heading('#0', text="Name")
+        tv.heading('countryCode', text='Country Code')
+        tv.heading('productCode', text='Product Code')
+        tv.heading('identifier', text='Identifier')
+        self.tv = tv
+        self.tv.grid_rowconfigure(0, weight = 1)
+        self.tv.grid_columnconfigure(0, weight = 1)
+        self.tv.pack(side=tk.TOP,fill=tk.X)
+       
 
-    root.config(menu=menu)
     
-   
-    folderPathLabel.place(relx=1.0, rely=1.0, anchor='se')
-
-loadConfig()
+    def updateTable(self, file, countryCode, productCode, identifier):
+        self.tv.insert('', 'end', text=file, values=(countryCode, productCode, identifier))
 
 
-window(root)
-root.mainloop()
+        
 
+    # def updateComponents(self):
+    #     self.master.update_idletasks()
+    #     self.master.after(1000, self.updateComponents)
+
+gui = App()
+loadConfig(gui.folderPath)
+gui.master.mainloop()
